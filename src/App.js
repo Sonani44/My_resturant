9d12832b873f76dd583aece90c67a3ad22533fdf
Sonani Mohapatra <sonanimohapatra015@gmail.com>
2024-04-23 20:37:59 +0530

    full project uploaded

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Resturant from './component/Basics/Resturant'
+function App() {
+  return (
+    <div>
+      <Resturant/>
+    </div>
+  )
+}
+
+export default App
